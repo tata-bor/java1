812b150a6edc7ffe48a7409643d9a50414c28f74
tata-bor <tatiana.bormatova@gmail.com>
2021-02-09 20:43:45 +0300

    ДЗ Пункт 2

---
+++ src/HomeWork.java
@@ -0,0 +1,13 @@
+public class HomeWork {
+    public static void initialize () {
+        byte byteValue = -110;
+        boolean booleanValue = true;
+        short shortValue = 1123;
+        long longValue = 100000L;
+        int intValue = 45677;
+        double doubleValue = 12.12;
+        float floatValue = 10000f;
+        char charValue = '*';
+    }
+    }
+}
