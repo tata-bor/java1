f82101b72b5b6ca76a21dfda1d014bdaf7f2a39d
tata-bor <tatiana.bormatova@gmail.com>
2021-02-22 19:58:53 +0300

    Урок 2, задание 3

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class HomeWork2 {
-  // Задание 1
+    // Задание 1
     public static void SetArray() {
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < 10; i++) {
@@ -12,19 +12,32 @@ public class HomeWork2 {
             }
         }
         for (int i = 0; i < 10; i++) {
-            System.out.print (arr [i] + " ");
+            System.out.print(arr[i] + " ");
         }
     }
 
     //Задание 2
 
     public static void SetArray2() {
-        int arr2 [] = new int [8];
+        int arr2[] = new int[8];
         for (int i = 0; i < arr2.length; i++) {
-            arr2 [i] = i * 3;
+            arr2[i] = i * 3;
         }
         for (int i = 0; i < arr2.length; i++) {
-            System.out.print (arr2 [i] + " ");
+            System.out.print(arr2[i] + " ");
+        }
+    }
+
+    // Задание 3
+    public static void SetArray3() {
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < 12; i++) {
+            if (arr3[i] < 6) {
+                arr3[i] = arr3[i] * 2;
+            }
+        }
+        for (int i = 0; i < 12; i++) {
+            System.out.print(arr3[i] + " ");
         }
     }
 }
