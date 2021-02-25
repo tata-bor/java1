774d8bbd5a2f9eafac72015115634a9723976448
tata-bor <tatiana.bormatova@gmail.com>
2021-02-25 19:16:48 +0300

    Урок 4, выполнила 1 и 2 задания

---
+++ src/Homework3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*public class Homework3 {
+    public static void Game1(String[] args) {
+        System.out.println("Задача игры: угадать число.");
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < 9; i += 3) {
+        }
+        int number = (int) (Math.random(i));
+        while (true) {
+            System.out.println("Отгадайте число от 0 до " + i);
+            int input_number = in.nextInt();
+            if (input_number == number) {
+                System.out.println("Поздравляем! Вы угадали!");
+                return;
+            }
+
+            if (input_number > number) {
+                System.out.println("Загаданное число меньше.");
+            } else {
+                System.out.println("Загаданное число больше.");
+            }
+        }
+    }
+}*/
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        var hw = new HomeWork4();
+        hw.Run();
+       /*System.out.println("Hello, world!");*/
+
     }
 }
+
 
