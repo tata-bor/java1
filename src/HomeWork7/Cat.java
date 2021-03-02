d12cdc533d9fd1a31c106f338844588e5515b4df
tata-bor <tatiana.bormatova@gmail.com>
2021-03-02 20:12:55 +0300

    Урок 7, задание 5

--- src/HomeWork7/Cat.java
+++ src/HomeWork7/Cat.java
@@ -4,11 +4,13 @@ public class Cat {
     private String name;
     private int appetite;
     private boolean satiety;
+
     public Cat(String name, int appetite) {
         this.name = name;
         this.appetite = appetite;
         this.satiety = false;
     }
+
     public void eat(Plate p) {
         boolean catEat = p.decreaseFood(appetite);
         if (catEat) {
@@ -16,5 +18,14 @@ public class Cat {
         }
         System.out.println("Сытость кота = " + satiety);
     }
+
+    public void print() {
+        if (satiety) {
+            System.out.println("Кот " + name + " сыт");
+        } else {
+            System.out.println("Кот " + name + " голоден");
+        }
+    }
 }
+
 
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,23 @@ import HomeWork7.Plate;
 
 public class Main {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
+        /* Cat cat = new Cat("Barsik", 5);
         Plate plate = new Plate(100);
         plate.info();
         cat.eat(plate);
-        plate.info();
+        plate.info(); */
+        Plate plate = new Plate(20);
+        Cat[] catArray = new Cat[5];
+        catArray[0] = new Cat("Tom", 10);
+        catArray[1] = new Cat("Jerry", 5);
+        catArray[2] = new Cat("Matilda", 12);
+        catArray[3] = new Cat("Boris", 6);
+        catArray[4] = new Cat("Vasil", 8);
+
+        for (int i = 0; i < 5; i++) {
+            catArray[i].eat(plate);
+            catArray[i].print();
+        }
     }
-    }
+}
 
